087c0a2d8adefba823427468e9a39d11cb303663
saragl728 <saragl728@gmail.com>
2023-03-10 13:53:51 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_SaraGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
