03fa8b21ab3a4f7373d4b74eb1ce230a02024470
saragl728 <saragl728@gmail.com>
2023-03-10 14:02:51 +0100

    Se borra el código inicial y se ponen  comentarios

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //CONSTANTES
+
+            //VARIABLES
+
+            //INICIALIZACIÓN
+
+            //ENTRADA
+
+            //PROCESO
+
+            //SALIDA
         }
     }
 }
